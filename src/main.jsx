d448be38aca45a1ce21d9fb501c301bb72485663
Amir Afshari <realamirafshari@gmail.com>
2025-03-26 00:06:43 +0430

    fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./setting.css";
@@ -20,4 +8,4 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
->>>>>>> 8108419 (develope UI)
+
